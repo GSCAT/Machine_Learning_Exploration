3025a8932c75ef0be352779350cf9ccc9da6664f
Kevin Bonds <Kevin_Bonds@gap.com>
2017-01-05 15:42:10 -0800

    Add SQL_Example

---
+++ SQL_Example.R
@@ -0,0 +1,14 @@
+library(dplyr)
+library(readr)
+library(RSQLServer)
+library(RODBC)
+library(formattable)
+library(RJDBC)
+library(rChoiceDialogs)
+library(lubridate)
+library(RCurl)
+
+# Create RODBC connection---- 
+my_connect <- odbcConnect(dsn= "IP EDWP", uid= my_uid, pwd= my_pwd)
+# sqlTables(my_connect, catalog = "EDWP", tableName  = "tables")
+sqlQuery(my_connect, query = "SELECT  * from dbc.dbcinfo;")
